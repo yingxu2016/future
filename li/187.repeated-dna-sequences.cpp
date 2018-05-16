8ad80ee2bf40fe0eae7bf4bb0ca4ba1b7dd85593
ziqif <ziqifan16@gmail.com>
2018-05-16 16:26:23 -0700

    more

--- li/187.repeated-dna-sequences.cpp
+++ li/187.repeated-dna-sequences.cpp
@@ -15,3 +15,32 @@ public:
         return vector<string>(ans.begin(), ans.end());
     }
 };
+
+// bit encoding
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        vector<string> res;
+        if(s.size() < 10)
+            return res;
+        unordered_map<int, int> DNA;
+        unordered_map<char, int> types;
+        types['A'] = 0;
+        types['C'] = 1;
+        types['G'] = 2;
+        types['T'] = 3;
+        int key = 0;
+        for(int i = 0; i<s.size(); i++) {
+            key = ((key << 2) | types[s[i]]) & 0xfffff;
+            if(i < 9)
+                continue;
+            if(DNA.find(key) == DNA.end())
+                DNA[key] = 1;
+            else if(DNA[key] == 1) {
+                DNA[key] = 2;
+                res.push_back(s.substr(i-9, 10));
+            }
+        }
+        return res;
+    }
+};
