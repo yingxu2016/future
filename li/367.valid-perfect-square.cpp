6474554ce1228faa9163047c9b019cc7aa91c96f
ziqif <ziqifan16@gmail.com>
2018-05-24 16:28:37 -0700

    more

--- li/367.valid-perfect-square.cpp
+++ li/367.valid-perfect-square.cpp
@@ -3,12 +3,14 @@ public:
     bool isPerfectSquare(int num) {
         long l = 1;
         long r = num;
-        while(l <= r) {
+        while(l + 1 < r) {
             long mid = (l + r) >> 1;
             if(mid * mid == num) return true;
-            else if(mid * mid > num) r = mid - 1;
-            else l = mid + 1;
+            else if(mid * mid > num) r = mid;
+            else l = mid;
         }
+        if(r * r == num) return r;
+        if(l * l == num) return l;
         return false;
     }
 };
