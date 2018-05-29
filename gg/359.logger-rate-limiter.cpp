2c6f52fea6a972d53cc7752d52ebdd3c032107c6
ziqif <ziqifan16@gmail.com>
2018-05-29 12:31:26 -0700

    more

---
+++ gg/359.logger-rate-limiter.cpp
@@ -0,0 +1,33 @@
+class Logger {
+public:
+    /** Initialize your data structure here. */
+    Logger() {
+
+    }
+
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        if(!mp.count(message)) {
+            mp[message] = timestamp;
+            return true;
+        }
+        else {
+            if(timestamp - mp[message] >= 10) {
+                mp[message] = timestamp;
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    unordered_map<string, int> mp;
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * bool param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
