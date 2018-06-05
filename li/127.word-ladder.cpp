21282cfd85b1361b33a5f0d9c81025da53c0caca
ziqif <ziqifan16@gmail.com>
2018-06-05 16:47:57 -0700

    more

--- li/127.word-ladder.cpp
+++ li/127.word-ladder.cpp
@@ -34,3 +34,41 @@ public:
         return 0;
     }
 };
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        if (!dict.count(endWord)) return 0;
+
+        queue<string> q;
+        q.push(beginWord);
+
+        int l = beginWord.length();
+        int step = 0;
+
+        while (!q.empty()) {
+            ++step;
+            for (int size = q.size(); size > 0; size--) {
+                string w = q.front();
+                q.pop();
+                for (int i = 0; i < l; i++) {
+                    char ch = w[i];
+                    for (int j = 'a'; j <= 'z'; j++) {
+                        w[i] = j;
+                        // Found the solution
+                        if (w == endWord) return step + 1;
+                        // Not in dict, skip it
+                        if (!dict.count(w)) continue;
+                        // Remove new word from dict
+                        dict.erase(w);
+                        // Add new word into queue
+                        q.push(w);
+                    }
+                    w[i] = ch;
+                }
+            }
+        }
+        return 0;
+    }
+};
