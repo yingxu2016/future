958e04b45be9012b7afb82ab8405c304cda05c58
ziqif <ziqifan16@gmail.com>
2018-06-04 18:22:19 -0700

    more

--- li/236.lowest-common-ancestor-of-a-binary-tree.cpp
+++ li/236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -25,3 +25,41 @@ public:
     }
 };
 
+// Note this solution can deal if the target node(s) are NOT in the tree
+class Solution2 {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* one, TreeNode* two) {
+        TreeNode* ans = lca(root, one, two);
+        if (ans == one) {
+            return find(one, two) == true ? ans : NULL;
+        } else if (ans == two) {
+            return find(two, one) == true ? ans : NULL;
+        }
+        return ans;
+    }
+
+private:
+    TreeNode* lca(TreeNode* root, TreeNode* one, TreeNode* two) {
+        if (root == NULL || root == one || root == two) {
+            return root;
+        }
+        TreeNode* left = lca(root->left, one, two);
+        TreeNode* right = lca(root->right, one, two);
+        if (left != NULL && right != NULL) {
+            return root;
+        }
+        return left == NULL ? right : left;
+    }
+
+    // check if node is under root
+    bool find(TreeNode* root, TreeNode* node) {
+        if (root == NULL) {
+            return false;
+        }
+        if (root == node) {
+            return true;
+        }
+        return find(root->left, node) || find(root->right, node);
+    }
+};
+
