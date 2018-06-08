c97158c5831f7289dc8e0c7ace8a8ae8e9ffffb0
ziqif <ziqifan16@gmail.com>
2018-06-07 18:10:36 -0700

    more

--- li/653.two-sum-iv-input-is-a-bst.cpp
+++ li/653.two-sum-iv-input-is-a-bst.cpp
@@ -1,12 +1,71 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+// Time: O(n)
+// Space: O(log(n))
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if (root == NULL) {
+            return false;
+        }
+
+        stack<TreeNode*> leftSide;
+        stack<TreeNode*> rightSide;
+
+        // Find the greatest and smallest Node in the Tree first.
+        TreeNode* tmp = root;
+        while (tmp != NULL) {
+            leftSide.push(tmp);
+            tmp = tmp->left;
+        }
+        tmp = root;
+        while (tmp != NULL) {
+            rightSide.push(tmp);
+            tmp = tmp->right;
+        }
+
+        // Then perform the same 2 pointer idea like we are facing a sorted list
+        // However, in this case, we always try to traverse the tree to looking
+        // for the next smaller or larger TreeNode. This is applicable since
+        // this is a BST.
+        TreeNode* left = leftSide.top();
+        leftSide.pop();
+        TreeNode* right = rightSide.top();
+        rightSide.pop();
+
+        while (left->val != right->val) {
+            // If there is no redundant number in the Tree,
+            // then left.val == right.val can indicates a thorough search is completed.
+            int currSum = left->val + right->val;
+            if (currSum == k) {
+                return true;
+            } else if (currSum < k) {
+                if (left->right != NULL) {
+                    left = left->right;
+                    while (left != NULL) {
+                        leftSide.push(left);
+                        left = left->left;
+                    }
+                }
+                left = leftSide.top();
+                leftSide.pop();
+            } else { // currSum > k
+                if (right->left != NULL) {
+                    right = right->left;
+                    while (right != NULL) {
+                        rightSide.push(right);
+                        right = right->right;
+                    }
+                }
+                right = rightSide.top();
+                rightSide.pop();
+            }
+        }
+
+        return false;
+    }
+};
+
+// Time: O(n)
+// Space: O(n)
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
@@ -36,3 +95,13 @@ public:
         helper(root->right, v);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
