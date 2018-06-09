40001e66d70eabae1fecc992e87c0fddbee51b0b
Ziqi Fan <ziqifan16@gmail.com>
2018-06-09 14:43:11 -0700

    more

--- li/377.combination-sum-iv.cpp
+++ li/377.combination-sum-iv.cpp
@@ -5,15 +5,15 @@ class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
         m_ = vector<int>(target + 1, -1);
-        m_[0] = 1;
         return dp(nums, target);
     }
 private:
     int dp(const vector<int>& nums, int target) {
-        if (target < 0) return 0;
-        if (m_[target] != -1) return m_[target];
+        if(target < 0) return 0;
+        if(target == 0) return 1;
+        if(m_[target] != -1) return m_[target];
         int ans = 0;
-        for (const int num : nums)
+        for(const int num : nums)
             ans += dp(nums, target - num);
         return m_[target] = ans;
     }
