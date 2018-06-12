8f18cc2ec07b4400b124a7ae415265e6287f5fca
ziqif <ziqifan16@gmail.com>
2018-06-12 16:34:05 -0700

    more

---
+++ fb/206.reverse-linked-list.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+// Time: O(n)
+// Space: O(1)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head) return NULL;
+        ListNode* pre = NULL;
+        while(head) {
+            ListNode* tmp = head->next;
+            head->next = pre;
+            pre = head;
+            head = tmp;
+        }
+        return pre;
+    }
+};
+
+// Time: O(n)
+// Space: O(n)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* root = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return root;
+    }
+};
