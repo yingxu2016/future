3c4aacc3c11af84386afd48c6c9726ccc0c47953
ziqif <ziqifan16@gmail.com>
2018-06-20 19:28:13 -0700

    more

---
+++ fb/257.binary-tree-paths.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        if(!root) return ans;
+        binaryTreePaths(root, ans, "");
+        return ans;
+    }
+
+private:
+    void binaryTreePaths(TreeNode* root, vector<string>& paths, string path) {
+        if(!root->left && !root->right) {
+            path += to_string(root->val);
+            paths.push_back(path);
+            return;
+        }
+        path += to_string(root->val) + "->";
+        if(root->left)
+            binaryTreePaths(root->left, paths, path);
+        if(root->right)
+            binaryTreePaths(root->right, paths, path);
+    }
+};
