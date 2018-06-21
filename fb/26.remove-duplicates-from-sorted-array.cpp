6c6bc77cc3fa7c36d784b3d5b50fe36f4039374b
ziqif <ziqifan16@gmail.com>
2018-06-21 11:08:32 -0700

    more

---
+++ fb/26.remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int cur = 0;
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] != nums[cur]) {
+                nums[++cur] = nums[i];
+            }
+        }
+        return cur + 1;
+    }
+};
