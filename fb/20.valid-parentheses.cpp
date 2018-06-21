a01ec0e96596d8ff1f80447d1bc4786c5c90eb14
ziqif <ziqifan16@gmail.com>
2018-06-21 14:35:36 -0700

    more

---
+++ fb/20.valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(auto c : s) {
+            if(c == '(' || c == '{' || c == '[')
+                stk.push(c);
+            else {
+                if(stk.empty()) return false;
+                if(c == ')') {
+                    char t = stk.top();
+                    stk.pop();
+                    if(t != '(') return false;
+                }
+                else if(c == ']') {
+                    char t = stk.top();
+                    stk.pop();
+                    if(t != '[') return false;
+                }
+                else {
+                    char t = stk.top();
+                    stk.pop();
+                    if(t != '{') return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
