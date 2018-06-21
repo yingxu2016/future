cfe915c7c1024c15af14774b43808b9b5005a5a7
ziqif <ziqifan16@gmail.com>
2018-06-20 17:41:35 -0700

    more

---
+++ fb/543.diameter-of-binary-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root) return 0;
+        int ans = INT_MIN;
+        helper(root, ans);
+        return ans;
+    }
+
+private:
+    int helper(TreeNode* root, int& ans) {
+        if(!root) return -1;
+        int l = root->left ? helper(root->left, ans) + 1 : 0;
+        int r = root->right ? helper(root->right, ans) + 1 : 0;
+        ans = max(ans, l + r);
+        return max(l, r);
+    }
+};
