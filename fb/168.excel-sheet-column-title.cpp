0487b008f822e034e3e1ff1f5fd625dcd0376db8
Ziqi Fan <ziqifan16@gmail.com>
2018-06-24 15:59:51 -0700

    more

---
+++ fb/168.excel-sheet-column-title.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string str = "";
+        while(n) {
+            str = char((n - 1) % 26 + 'A') + str;
+            n = (n  - 1) / 26;
+        }
+        return str;
+    }
+};
