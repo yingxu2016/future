15f039db326fd5282f297595d55e691cd83dcebc
ziqif <ziqifan16@gmail.com>
2018-07-03 16:26:34 -0700

    more

---
+++ fb/670.maximum-swap.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string digits = to_string(num);
+
+        vector<int> buckets(10);
+        for (int i = 0; i < digits.size(); i++) {
+            buckets[digits[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < digits.size(); i++) {      //从最高位开始
+            for (int k = 9; k > digits[i] - '0'; k--) {    // k需要比数字digits[i]大
+                if (buckets[k] > i) {     //如果k的位置在i后面
+                    swap(digits[buckets[k]], digits[i]);
+                    return stoi(digits);
+                }
+            }
+        }
+
+        return num;
+    }
+};
