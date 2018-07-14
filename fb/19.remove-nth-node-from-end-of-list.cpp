f0bdcdad702ef938fb0dedccc4a004741bad9e84
Ziqi Fan <ziqifan16@gmail.com>
2018-07-14 14:08:01 -0700

    19

---
+++ fb/19.remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy{0};
+        dummy.next = head;
+        ListNode* p = &dummy, *q = &dummy;
+        while (n--) p = p->next;
+        while (p->next != nullptr) {
+            p = p->next, q = q->next;
+        }
+        q->next = q->next->next;
+        return dummy.next;
+    }
+};
