cc0d309289cadfe319f1603536f2a689a3e09d1d
Ziqi Fan <ziqifan16@gmail.com>
2018-07-14 13:41:52 -0700

    91

--- fb/91.decode-ways.cpp
+++ fb/91.decode-ways.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int ways_1 = 0, ways_2 = 0, ways = 0; // read as ways minus 1, ways minus 2 and ways
-        if (s.empty() || s[0] == '0')
-            return 0;
-        ways_1 = ways_2 = ways = 1;
-        for (int i = 1; i < s.size(); ++i) {
-            if (s[i] == '0') {
-                ways = (s[i-1] == '1' || s[i-1] == '2') ? ways_2 : 0;
-            } else {
-                ways = (s[i-1] == '1' || (s[i-1] == '2' && s[i] <'7')) ?
-                       (ways_1 + ways_2) : ways_1;
-            }
-            ways_2 = ways_1;
-            ways_1 = ways;
+        if (s.empty() || s[0] == '0') return 0;
+        if (s.length() == 1) return 1;
+
+        const int n = s.length();
+        int w1 = 1;
+        int w2 = 1;
+        for (int i = 1; i < n; ++i) {
+            int w = 0;
+            if (!isValid(s[i]) && !isValid(s[i - 1], s[i])) return 0;
+            if (isValid(s[i])) w = w1;
+            if (isValid(s[i - 1], s[i])) w += w2;
+            w2 = w1;
+            w1 = w;
         }
-        return ways;
+        return w1;
+    }
+private:
+    bool isValid(const char c) {
+        return c != '0';
+    }
+    bool isValid(const char c1, const char c2) {
+        const int num = (c1 - '0')*10 + (c2 - '0');
+        return num >= 10 && num <= 26;
     }
 };
