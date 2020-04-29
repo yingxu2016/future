afc4dde64177b283fdf40ed505d65723a51de201
BEaFAN <ziqifan16@gmail.com>
2020-04-29 15:59:35 -0700

    Update 253.meeting-rooms-ii.cpp

--- li/253.meeting-rooms-ii.cpp
+++ li/253.meeting-rooms-ii.cpp
@@ -1,3 +1,25 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        if(intervals.empty()) {
+            return 0;
+        }
+        sort(intervals.begin(), intervals.end());
+        auto comp = [](const vector<int>& a, const vector<int>& b) {
+            return a[1] > b[1];
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);
+        int s = intervals.size();
+        for(int i = 0; i < s; i++) {
+            if (!pq.empty() && pq.top()[1] <= intervals[i][0]) {
+                pq.pop();
+            }
+            pq.push(intervals[i]);
+        }
+        return pq.size();
+    }
+};
+
 // Source: https://www.youtube.com/watch?v=jUpuIio_oYo
 class Solution {
 public:
@@ -18,31 +40,6 @@ public:
             else {
                 end++;
             }
-        }
-        return ans;
-    }
-};
-
-// Author: Ziqi
-class Solution2 {
-public:
-    int minMeetingRooms(vector<vector<int>>& intervals) {
-        if(intervals.empty()) {
-            return 0;
-        }
-        int ans = 0;
-        sort(intervals.begin(), intervals.end());
-        auto comp = [](const vector<int>& a, const vector<int>& b) {
-            return a[1] > b[1];
-        };
-        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);
-        int s = intervals.size();
-        for(int i = 0; i < s; i++) {
-            while(!pq.empty() && pq.top()[1] <= intervals[i][0]) {
-                pq.pop();
-            }
-            pq.push(intervals[i]);
-            ans = max(ans, (int)pq.size());
         }
         return ans;
     }
