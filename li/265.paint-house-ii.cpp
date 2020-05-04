243b73a85b822c3dc718fd304064b3effc9799a1
BEaFAN <ziqifan16@gmail.com>
2020-05-04 15:10:13 -0700

    Update 265.paint-house-ii.cpp

--- li/265.paint-house-ii.cpp
+++ li/265.paint-house-ii.cpp
@@ -1,3 +1,47 @@
+// Time: O(k*n); Space: O(1)
+class Solution {
+public:
+    int minCostII(vector<vector<int>>& costs) {
+        if (costs.empty()) 
+            return 0;
+        if (costs[0].empty()) 
+            return 0;
+        int smallest = INT_MAX;
+        int smallest_index = -1;
+        int second_smallest = INT_MAX;
+        for (int i = 0; i < costs[0].size(); i++) {
+            if (costs[0][i] < smallest) {
+                second_smallest = smallest;
+                smallest = costs[0][i];
+                smallest_index = i;
+            } else if (costs[0][i] < second_smallest) {
+                second_smallest = costs[0][i];
+            }
+        }
+
+        for (int i = 1; i < costs.size(); i++) {
+            int _smallest = INT_MAX;
+            int _smallest_index = -1;
+            int _second_smallest = INT_MAX;
+            for (int j = 0; j < costs[i].size(); j++) {
+                int curr = costs[i][j] + (smallest_index == j ? second_smallest : smallest);
+                if (curr < _smallest) {
+                    _second_smallest = _smallest;
+                    _smallest = curr;
+                    _smallest_index = j;
+                } else if (curr < _second_smallest) {
+                    _second_smallest = curr;
+                }
+            }
+            smallest = _smallest;
+            smallest_index = _smallest_index;
+            second_smallest = _second_smallest; 
+        }
+        return smallest;        
+    }
+};
+
+// Time: O(kn); Space: O(k)
 class Solution {
 public:
     int minCostII(vector<vector<int>>& costs) {
