3c7279224ff63ac2752a6b92388bf863751f93db
BEaFAN <ziqifan16@gmail.com>
2020-05-27 13:13:10 -0700

    Update 40.combination-sum-ii.cpp

--- li/40.combination-sum-ii.cpp
+++ li/40.combination-sum-ii.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    vector<vector<int> > combinationSum2(vector<int> &num, int target)
-    {
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> res;
-        sort(num.begin(),num.end());
+        sort(candidates.begin(), candidates.end());
         vector<int> local;
-        findCombination(res, 0, target, local, num);
+        findCombination(res, 0, target, local, candidates);
         return res;
     }
-    void findCombination(vector<vector<int>>& res, const int order, const int target, vector<int>& local, const vector<int>& num)
-    {
-        if(target==0)
-        {
+    
+private:
+    void findCombination(vector<vector<int>>& res, const int order, const int target, vector<int>& local, const vector<int>& candidates) {
+        if(target==0) {
             res.push_back(local);
             return;
         }
-        else
-        {
-            for(int i = order; i<num.size(); i++) // iterative component
-            {
-                if(num[i]>target) return;
-                if(i != order && num[i]==num[i-1]) continue; // check duplicate combination
-                local.push_back(num[i]);
-                findCombination(res,i+1,target-num[i],local,num); // recursive componenet
-                local.pop_back();
-            }
+        for(int i = order; i<candidates.size(); i++) {
+            if(candidates[i] > target) return;
+            if(i != order && candidates[i] == candidates[i-1]) continue; // check duplicate combination
+            local.push_back(candidates[i]);
+            findCombination(res, i + 1, target - candidates[i] ,local, candidates); // recursive componenet
+            local.pop_back();
         }
     }
 };
