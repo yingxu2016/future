6d1003dfe855ed8769b78b2071139eb5d150e001
BEaFAN <ziqifan16@gmail.com>
2020-06-10 12:10:45 -0700

    Update 236.lowest-common-ancestor-of-a-binary-tree.cpp

--- li/236.lowest-common-ancestor-of-a-binary-tree.cpp
+++ li/236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,17 +10,11 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root || root == p || root == q) {
-            return root;
-        }
+        if(!root || root == p || root == q) return root;
         TreeNode* l = lowestCommonAncestor(root->left, p, q);
         TreeNode* r = lowestCommonAncestor(root->right, p, q);
-        if(l && r) {
-            return root;
-        }
-        else if (l) {
-            return l;
-        }
+        if(l && r) return root;
+        if(l) return l;
         return r;
     }
 };
