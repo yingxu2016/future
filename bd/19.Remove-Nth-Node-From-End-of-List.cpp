abe9920bc5e79a618c70f90b2dbb7f8ea5b75f42
BEaFAN <ziqifan16@gmail.com>
2020-06-14 13:01:02 -0700

    Update 19.Remove-Nth-Node-From-End-of-List.cpp

--- bd/19.Remove-Nth-Node-From-End-of-List.cpp
+++ bd/19.Remove-Nth-Node-From-End-of-List.cpp
@@ -1,3 +1,25 @@
+// One pass
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        ListNode* first = dummy;
+        ListNode* second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        for (int i = 0; i <= n; i++) {
+            first = first->next;
+        }
+        // Move first to the end, maintaining the gap
+        while (first != NULL) {
+            first = first->next;
+            second = second->next;
+        }
+        second->next = second->next->next;
+        return dummy->next;
+    }
+};
+
 // Two pass
 class Solution {
 public:
