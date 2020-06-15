b37f45e97a8ab58a8237bcf0645b2dfe09ecb0c4
BEaFAN <ziqifan16@gmail.com>
2020-06-15 14:21:06 -0700

    Update 605.can-place-flowers.cpp

--- li/605.can-place-flowers.cpp
+++ li/605.can-place-flowers.cpp
@@ -14,3 +14,21 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if(n == 0) return true;
+        int s = flowerbed.size();
+        for(int i = 0; i < s; i++) {
+            int pre = i == 0 ? 0 : flowerbed[i-1];
+            int next = i == s - 1 ? 0 : flowerbed[i+1]; 
+            if(flowerbed[i] == 0 && pre == 0 && next == 0) {
+                flowerbed[i] = 1;
+                n--;
+                if(n == 0) return true;
+            }
+        }
+        return false;    
+    }
+};
