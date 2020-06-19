4eae4210c7f629effc18a5aac58dd261ba9f1a12
BEaFAN <ziqifan16@gmail.com>
2020-06-19 00:51:49 -0700

    Update Diameter-of-a-Binary-Tree.c

--- bd/Diameter-of-a-Binary-Tree.c
+++ bd/Diameter-of-a-Binary-Tree.c
@@ -11,30 +11,30 @@ struct node *root = SomeFunctionToMakeTree();
 int diameter = diameterOpt(root, &height); */
 int diameterOpt(struct node *root, int* height) 
 { 
-/* lh --> Height of left subtree 
+	/* lh --> Height of left subtree 
 	rh --> Height of right subtree */
-int lh = 0, rh = 0; 
+	int lh = 0, rh = 0; 
 
-/* ldiameter --> diameter of left subtree 
+	/* ldiameter --> diameter of left subtree 
 	rdiameter --> Diameter of right subtree */
-int ldiameter = 0, rdiameter = 0; 
+	int ldiameter = 0, rdiameter = 0; 
 
-if (root == NULL) 
-{ 
-	*height = 0; 
-	return 0; /* diameter is also 0 */
-} 
+	if (root == NULL) 
+	{ 
+		*height = 0; 
+		return 0; /* diameter is also 0 */
+	} 
 
-/* Get the heights of left and right subtrees in lh and rh 
-	And store the returned values in ldiameter and ldiameter */
-ldiameter = diameterOpt(root->left, &lh); 
-rdiameter = diameterOpt(root->right, &rh); 
+	/* Get the heights of left and right subtrees in lh and rh 
+		And store the returned values in ldiameter and ldiameter */
+	ldiameter = diameterOpt(root->left, &lh); 
+	rdiameter = diameterOpt(root->right, &rh); 
 
-/* Height of current node is max of heights of left and 
-	right subtrees plus 1*/
-*height = max(lh, rh) + 1; 
+	/* Height of current node is max of heights of left and 
+		right subtrees plus 1*/
+	*height = max(lh, rh) + 1; 
 
-return max(lh + rh + 1, max(ldiameter, rdiameter)); 
+	return max(lh + rh + 1, max(ldiameter, rdiameter)); 
 } 
 
 
@@ -62,23 +62,23 @@ int height(struct node* node);
 /* Function to get diameter of a binary tree */
 int diameter(struct node * tree) 
 { 
-/* base case where tree is empty */
-if (tree == NULL) 
-	return 0; 
+	/* base case where tree is empty */
+	if (tree == NULL) 
+		return 0; 
 
-/* get the height of left and right sub-trees */
-int lheight = height(tree->left); 
-int rheight = height(tree->right); 
+	/* get the height of left and right sub-trees */
+	int lheight = height(tree->left); 
+	int rheight = height(tree->right); 
 
-/* get the diameter of left and right sub-trees */
-int ldiameter = diameter(tree->left); 
-int rdiameter = diameter(tree->right); 
+	/* get the diameter of left and right sub-trees */
+	int ldiameter = diameter(tree->left); 
+	int rdiameter = diameter(tree->right); 
 
-/* Return max of following three 
-1) Diameter of left subtree 
-2) Diameter of right subtree 
-3) Height of left subtree + height of right subtree + 1 */
-return max(lheight + rheight + 1, max(ldiameter, rdiameter)); 
+	/* Return max of following three 
+	1) Diameter of left subtree 
+	2) Diameter of right subtree 
+	3) Height of left subtree + height of right subtree + 1 */
+	return max(lheight + rheight + 1, max(ldiameter, rdiameter)); 
 } 
 
 /* UTILITY FUNCTIONS TO TEST diameter() FUNCTION */
@@ -88,47 +88,46 @@ return max(lheight + rheight + 1, max(ldiameter, rdiameter));
 	down to the farthest leaf node.*/
 int height(struct node* node) 
 { 
-/* base case tree is empty */
-if(node == NULL) 
-	return 0; 
+	/* base case tree is empty */
+	if(node == NULL) 
+		return 0; 
 
-/* If tree is not empty then height = 1 + max of left 
-	height and right heights */	
-return 1 + max(height(node->left), height(node->right)); 
+	/* If tree is not empty then height = 1 + max of left 
+		height and right heights */	
+	return 1 + max(height(node->left), height(node->right)); 
 } 
 
 /* Helper function that allocates a new node with the 
 given data and NULL left and right pointers. */
 struct node* newNode(int data) 
 { 
-struct node* node = (struct node*) 
-					malloc(sizeof(struct node)); 
-node->data = data; 
-node->left = NULL; 
-node->right = NULL; 
+	struct node* node = (struct node*) 
+						malloc(sizeof(struct node)); 
+	node->data = data; 
+	node->left = NULL; 
+	node->right = NULL; 
 
-return(node); 
+	return(node); 
 } 
 
 /* Driver program to test above functions*/
 int main() 
 { 
-
-/* Constructed binary tree is 
-			1 
+	/* Constructed binary tree is 
+				1 
+			/ \ 
+			2	 3 
 		/ \ 
-		2	 3 
-	/ \ 
-	4	 5 
-*/
-struct node *root = newNode(1); 
-root->left	 = newNode(2); 
-root->right	 = newNode(3); 
-root->left->left = newNode(4); 
-root->left->right = newNode(5); 
+		4	 5 
+	*/
+	struct node *root = newNode(1); 
+	root->left	 = newNode(2); 
+	root->right	 = newNode(3); 
+	root->left->left = newNode(4); 
+	root->left->right = newNode(5); 
 
-printf("Diameter of the given binary tree is %d\n", diameter(root)); 
+	printf("Diameter of the given binary tree is %d\n", diameter(root)); 
 
-return 0; 
+	return 0; 
 } 
 
