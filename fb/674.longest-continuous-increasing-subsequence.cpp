6fe5e455e8f27a5bd3cc4322ec99ca6c47e94d44
BEaFAN <ziqifan16@gmail.com>
2020-06-25 16:28:53 -0700

    Update 674.longest-continuous-increasing-subsequence.cpp

--- fb/674.longest-continuous-increasing-subsequence.cpp
+++ fb/674.longest-continuous-increasing-subsequence.cpp
@@ -3,8 +3,7 @@ public:
     int findLengthOfLCIS(vector<int>& nums) {
         if(nums.empty()) return 0;
         int ans = 1;
-        int cur = 1;
-        for(int i = 1; i < nums.size(); i++) {
+        for(int i = 1, cur = 1; i < nums.size(); i++) {
             if(nums[i] > nums[i-1]) {
                 cur++;
                 ans = max(ans, cur);
