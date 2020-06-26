fc6a3724dd32e9d7a9a2f5c7f70b074d50c01c09
BEaFAN <ziqifan16@gmail.com>
2020-06-25 22:13:38 -0700

    Update 173.binary-search-tree-iterator.cpp

--- fb/173.binary-search-tree-iterator.cpp
+++ fb/173.binary-search-tree-iterator.cpp
@@ -1,47 +1,52 @@
+// Time O(n)
+// Space O(h) - h is tree height
+class BSTIterator {
+private:
+    stack<TreeNode*> stk;
+    
+    void leftmostInorder(TreeNode* root) {
+        // For a given node, add all the elements in the leftmost branch of the tree
+        // under it to the stack.
+        while(root) {
+            stk.push(root);
+            root = root->left;
+        }
+    }
+public:
+    BSTIterator(TreeNode* root) {
+        leftmostInorder(root);
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        assert(!stk.empty());
+        TreeNode* node = stk.top();
+        stk.pop();
+        leftmostInorder(node->right);
+        return node->val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !stk.empty();
+    }
+};
+
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class BSTIterator {
-public:
-    BSTIterator(TreeNode *root) {
-        if(!root) return;
-        while(root) {
-            s.push(root);
-            root = root->left;
-        }
-    }
-
-    /** @return whether we have a next smallest number */
-    bool hasNext() {
-        return !s.empty();
-    }
-
-    /** @return the next smallest number */
-    int next() {
-        TreeNode* n = s.top();
-        int ans = n->val;
-        s.pop();
-        if(n->right) {
-            n = n->right;
-            while(n) {
-                s.push(n);
-                n = n->left;
-            }
-        }
-        return ans;
-    }
-private:
-    stack<TreeNode*> s;
-};
 
 /**
- * Your BSTIterator will be called like this:
- * BSTIterator i = BSTIterator(root);
- * while (i.hasNext()) cout << i.next();
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
  */
