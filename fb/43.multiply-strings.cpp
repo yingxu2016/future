f78f5a249294ab8cf3c187b16ab0f51e8ef3aea2
BEaFAN <ziqifan16@gmail.com>
2020-06-26 10:45:00 -0700

    Update 43.multiply-strings.cpp

--- fb/43.multiply-strings.cpp
+++ fb/43.multiply-strings.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        string s(1000,'0');  
+        string s(220,'0');  
         reverse(num1.begin(),num1.end());  
         reverse(num2.begin(),num2.end());  
         for(int i=0;i<num1.length();i++)  
             for(int j=0;j<num2.length();j++)  
             {  
                 int temp=(num1[i]-'0')*(num2[j]-'0');  
-                s[i+j+1]=s[i+j+1]-'0'+(s[i+j]-'0'+temp)/10+'0';  
-                s[i+j]=(s[i+j]-'0'+temp)%10+'0';  
+                s[i+j+1]=s[i+j+1]-'0'+(s[i+j]-'0'+temp)/10+'0'; // calculate carry
+                s[i+j]=(s[i+j]-'0'+temp)%10+'0';  // calculate base
             }  
         reverse(s.begin(),s.end());  
         if(s.find_first_not_of('0') == string::npos)  
