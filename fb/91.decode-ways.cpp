a2d6a7dc915ba8af06573709d0e68d2e2346d8ab
BEaFAN <ziqifan16@gmail.com>
2020-06-26 15:35:45 -0700

    Update 91.decode-ways.cpp

--- fb/91.decode-ways.cpp
+++ fb/91.decode-ways.cpp
@@ -1,3 +1,5 @@
+// Time O(n)
+// Space O(1)
 class Solution {
 public:
     int numDecodings(string s) {
@@ -24,5 +26,26 @@ private:
     bool isValid(const char c1, const char c2) {
         const int num = (c1 - '0')*10 + (c2 - '0');
         return num >= 10 && num <= 26;
+    }
+};
+
+// Time O(n)
+// Space O(n)
+class Solution {
+private:
+    unordered_map<string, int> memo;
+public:
+    int numDecodings(string s) {
+        if(s.empty()) return 1;
+        if(s[0] == '0') return 0;
+        if(s.size() == 1) return 1;
+        if(memo.find(s) != memo.end()) return memo[s];
+        int ans = 0;
+        int num = stoi(s.substr(0, 2));
+        if(num <= 26)
+            ans += numDecodings(s.substr(2));
+        ans += numDecodings(s.substr(1));
+        memo[s] = ans;
+        return ans;
     }
 };
