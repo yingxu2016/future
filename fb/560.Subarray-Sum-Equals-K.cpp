7504075f036731a28e8dd1296a66736557adc3ed
BEaFAN <ziqifan16@gmail.com>
2020-06-28 21:26:59 -0700

    Create 560.Subarray-Sum-Equals-K.cpp

---
+++ fb/560.Subarray-Sum-Equals-K.cpp
@@ -0,0 +1,42 @@
+// Source: https://www.geeksforgeeks.org/number-subarrays-sum-exactly-equal-k/
+// Time O(n)
+// Space O(n)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        if(nums.empty()) return 0;
+        
+        // STL map to store number of subarrays 
+        // starting from index zero having  
+        // particular value of sum. 
+        unordered_map<int, int> prevSum; 
+
+        int res = 0; 
+
+        // Sum of elements so far. 
+        int currsum = 0;
+        
+        for (int i = 0; i < nums.size(); i++) { 
+            // Add current element to sum so far. 
+            currsum += nums[i]; 
+
+            // If currsum is equal to desired sum, 
+            // then a new subarray is found. So 
+            // increase count of subarrays. 
+            if (currsum == k)  
+                res++;         
+
+            // currsum exceeds given sum by (currsum - sum). 
+            // Find number of subarrays having this sum and exclude those
+            // subarrays from currsum by increasing count by same amount. 
+            if (prevSum.find(currsum - k) !=  prevSum.end())  
+                res += (prevSum[currsum - k]); 
+
+            // Add currsum value to count of  
+            // different values of sum. 
+            prevSum[currsum]++; 
+        } 
+        return res; 
+    }
+};
+
