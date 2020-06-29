8e31b3c1e6cac2eb8d23b118916e79808ad8e1dc
BEaFAN <ziqifan16@gmail.com>
2020-06-28 21:28:17 -0700

    Update 560.Subarray-Sum-Equals-K.cpp

--- fb/560.Subarray-Sum-Equals-K.cpp
+++ fb/560.Subarray-Sum-Equals-K.cpp
@@ -40,3 +40,21 @@ public:
     }
 };
 
+// Time O(n^2) - exceed time limit
+// Space O(1)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+       int count = 0;
+        for (int start = 0; start < nums.size(); start++) {
+            int sum=0;
+            for (int end = start; end < nums.size(); end++) {
+                sum+=nums[end];
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count; 
+    }
+};
+
