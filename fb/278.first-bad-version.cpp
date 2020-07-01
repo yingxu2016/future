38b3a7ceec99e34ffe5e46d121ec8a80e48db1b6
BEaFAN <ziqifan16@gmail.com>
2020-06-30 22:33:09 -0700

    Update 278.first-bad-version.cpp

--- fb/278.first-bad-version.cpp
+++ fb/278.first-bad-version.cpp
@@ -1,17 +1,21 @@
-// Forward declaration of isBadVersion API.
-bool isBadVersion(int version);
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
 
+// Time O(logn)
+// Space O(1)
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int l = 0, r = n;
-        while(l < r) {
-            int mid = l + (r - l) / 2;
-            if(isBadVersion(mid))
-                r = mid;
-            else
-                l = mid + 1;
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-        return r;
+        return left;
     }
 };
