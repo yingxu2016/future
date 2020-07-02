c569358a2a4be7c3345a2a625bcaf1cca4dfae79
BEaFAN <ziqifan16@gmail.com>
2020-07-02 11:31:31 -0700

    Update 295.Find-Median-from-Data-Stream.cpp

--- fb/295.Find-Median-from-Data-Stream.cpp
+++ fb/295.Find-Median-from-Data-Stream.cpp
@@ -23,3 +23,10 @@ public:
         return lo.size() > hi.size() ? lo.top() : ((double) lo.top() + hi.top()) * 0.5;
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
