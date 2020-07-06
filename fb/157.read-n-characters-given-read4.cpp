ef4da83ade3fd37e2cab7007861714213b561858
BEaFAN <ziqifan16@gmail.com>
2020-07-05 22:04:26 -0700

    Update 157.read-n-characters-given-read4.cpp

--- fb/157.read-n-characters-given-read4.cpp
+++ fb/157.read-n-characters-given-read4.cpp
@@ -1,6 +1,5 @@
-// Forward declaration of the read4 API.
-int read4(char *buf);
-
+// Time O(n)
+// Space O(1)
 class Solution {
 public:
     /**
@@ -19,5 +18,29 @@ public:
             ans += out;
         }
         return ans;
+    }
+};
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        int len = 0;
+        char buf4[4];
+        while(len < n) {
+            int ret = read4(buf4);
+            if(ret == 0) 
+                return len;
+            for(int i = 0; i < ret; i++) {
+                buf[len++] = buf4[i];
+                if(len == n)
+                    return len;
+            }
+        }
+        return len;
     }
 };
