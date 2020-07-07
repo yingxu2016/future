09a31e24f3cfec61041b0c41654cd32e0ee35422
BEaFAN <ziqifan16@gmail.com>
2020-07-07 16:33:47 -0700

    Update 235.lowest-common-ancestor-of-a-binary-search-tree.cpp

--- fb/235.lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ fb/235.lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,12 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+// Time O(n)
+// Space O(1)
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        int pVal = p->val;
+        int qVal = q->val;
+        TreeNode* node = root;
+        while(node) {
+            int parentVal = node->val;
+            if(pVal > parentVal && qVal > parentVal) {
+                node = node->right;
+            } else if (pVal < parentVal && qVal < parentVal) {
+                node = node->left;
+            } else {
+                return node;
+            }
+        }
+        return nullptr;
+    }
+};
+
+// Time O(n)
+// Space O(n)
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -18,3 +33,13 @@ public:
         return root;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
