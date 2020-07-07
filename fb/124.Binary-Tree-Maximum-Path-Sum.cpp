9dcef8b58b71c4d2c5d0eed1bce7e2f329568915
BEaFAN <ziqifan16@gmail.com>
2020-07-07 11:30:20 -0700

    Create 124.Binary-Tree-Maximum-Path-Sum.cpp

---
+++ fb/124.Binary-Tree-Maximum-Path-Sum.cpp
@@ -0,0 +1,40 @@
+// Time O(n)
+// Space O(H)
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int max_sum = INT_MIN;
+        max_gain(root, max_sum);
+        return max_sum;
+    }
+private:
+    int max_gain(TreeNode* node, int& max_sum) {
+        if (node == NULL) return 0;
+
+        // max sum on the left and right sub-trees of node
+        int left_gain = max(max_gain(node->left, max_sum), 0);
+        int right_gain = max(max_gain(node->right, max_sum), 0);
+
+        // the price to start a new path where `node` is a highest node
+        int price_newpath = node->val + left_gain + right_gain;
+
+        // update max_sum if it's better to start a new path
+        max_sum = max(max_sum, price_newpath);
+
+        // for recursion :
+        // return the max gain if continue the same path
+        return node->val + max(left_gain, right_gain);
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
