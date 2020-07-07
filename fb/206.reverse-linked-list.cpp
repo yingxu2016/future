2292496fdf8516f342f165e0e5206d8a32d2ffae
BEaFAN <ziqifan16@gmail.com>
2020-07-07 16:46:10 -0700

    Update 206.reverse-linked-list.cpp

--- fb/206.reverse-linked-list.cpp
+++ fb/206.reverse-linked-list.cpp
@@ -1,12 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-
 // Time: O(n)
 // Space: O(1)
 class Solution {
@@ -30,9 +21,18 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if(!head || !head->next) return head;
-        ListNode* root = reverseList(head->next);
+        ListNode* tail = reverseList(head->next);
         head->next->next = head;
         head->next = NULL;
-        return root;
+        return tail;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
