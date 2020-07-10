1e52e885e3e44299760f1392f40b20156ee2b7b7
BEaFAN <ziqifan16@gmail.com>
2020-07-09 21:46:06 -0700

    Update 128.longest-consecutive-sequence.cpp

--- fb/128.longest-consecutive-sequence.cpp
+++ fb/128.longest-consecutive-sequence.cpp
@@ -1,46 +1,23 @@
+// Time O(n)
+// Space O(n)
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.empty()) return 0;
-        unordered_set<int> num_set(nums.begin(), nums.end());
-        int ans = 0;
-        for(int num : num_set) {
-            if(!num_set.count(num-1)) {
+        unordered_set<int> numsSet(nums.begin(), nums.end());
+        int longestStreak = 0;
+        for(auto i : numsSet) {
+            // only start searching when i is the begining (i.e. smallest)
+            // of a sequence to avoid redundent searching
+            if(!numsSet.count(i - 1)) {
                 int currentStreak = 1;
-                while (num_set.count(++num)) {
-                    currentStreak += 1;
+                int curr = i + 1;
+                while(numsSet.count(curr)) {
+                    currentStreak++;
+                    curr++;
                 }
-                ans = max(ans, currentStreak);
+                longestStreak = max(currentStreak, longestStreak);
             }
         }
-        return ans;
-    }
-};
-
-// Author: Ziqi
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        if(nums.empty()) return 0;
-        unordered_set<int> s(nums.begin(), nums.end());
-        int ans = 0;
-        for(auto i : nums) {
-            int local = 1;
-            int tmp = i;
-            while(!s.empty() && s.count(--tmp)) {
-                local++;
-                s.erase(tmp);
-            }
-            tmp = i;
-            while(!s.empty() && s.count(++tmp)) {
-                local++;
-                s.erase(tmp);
-            }
-            s.erase(i);
-            ans = max(ans, local);
-            if(s.empty())
-                break;
-        }
-        return ans;
+        return longestStreak;
     }
 };
