bb2b99468eafca25e1791d39ecd19acd43df3488
BEaFAN <ziqifan16@gmail.com>
2020-07-10 12:13:47 -0700

    Create 2.Add-Two-Numbers.cpp

---
+++ fb/2.Add-Two-Numbers.cpp
@@ -0,0 +1,20 @@
+// Author: Huahua
+// Time O(max(l1,l2))
+// Space O(max(l1,l2))
+class Solution {
+public:
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+    int sum = 0;
+    while (l1 || l2 || sum) {
+      sum += (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+      l1 = l1 ? l1->next : nullptr;
+      l2 = l2 ? l2->next : nullptr;
+      tail->next = new ListNode(sum % 10);
+      sum /= 10;
+      tail = tail->next;
+    }            
+    return dummy.next;
+  }
+};
