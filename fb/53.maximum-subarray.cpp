60c93b4815d1398294857e8aae657f3c4bad0691
BEaFAN <ziqifan16@gmail.com>
2020-07-11 16:40:28 -0700

    Create 53.maximum-subarray.cpp

---
+++ fb/53.maximum-subarray.cpp
@@ -0,0 +1,18 @@
+// Time O(n)
+// Space O(1)
+// We need to convert this problem into two steps: find maxSunArrayEndingInNums[i] then find the largest, so
+// Step1: dp[i] denotes the largest sum of subarray ending with nums[i], i.e. must contain nums[i].
+// Step2: along the dp process, we need to find the largest dp[i], which will be the find answer.
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int ans = nums[0];
+        int dp = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            dp = max(dp + nums[i], nums[i]);
+            ans = max(ans, dp);
+        }
+        return ans;
+    }
+};
