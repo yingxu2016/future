ecd36208f8909bfde984f3351142dccf151735b3
BEaFAN <ziqifan16@gmail.com>
2020-07-12 16:28:34 -0700

    Create 437.Path-Sum-III.cpp

---
+++ fb/437.Path-Sum-III.cpp
@@ -0,0 +1,40 @@
+// Time O(n)
+// Space O(H)
+class Solution {
+public:
+  int pathSum(TreeNode* root, int sum) {
+    ans_ = 0;
+    sums_ = {{0, 1}};
+    pathSum(root, 0, sum);
+    return ans_;
+  }
+private:
+  int ans_;
+  unordered_map<int, int> sums_;
+  
+  void pathSum(TreeNode* root, int cur, int sum) {
+    if (!root) return;
+    cur += root->val;
+    ans_ += sums_[cur - sum];
+    ++sums_[cur];
+    pathSum(root->left, cur, sum);
+    pathSum(root->right, cur, sum);
+    --sums_[cur];
+  }
+};
+
+// Time O(n^2)
+// Space O(n)
+class Solution {
+public:
+  int pathSum(TreeNode* root, int sum) {
+    if (!root) return 0;
+    return numberOfPaths(root, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
+  }
+private:
+  int numberOfPaths(TreeNode* root, int left) {
+    if (!root) return 0;    
+    left -= root->val;
+    return (left == 0 ? 1 : 0) + numberOfPaths(root->left, left) + numberOfPaths(root->right, left);
+  }
+};
