2800dac384fe29abddb2b2dcc24aea5e670beb7f
BEaFAN <ziqifan16@gmail.com>
2020-07-13 16:43:54 -0700

    Update 246.Strobogrammatic-Number.cpp

--- fb/246.Strobogrammatic-Number.cpp
+++ fb/246.Strobogrammatic-Number.cpp
@@ -3,15 +3,15 @@
 class Solution {
 public:
     bool isStrobogrammatic(string num) {
-        unordered_map<char, char> um = {{'0', '0'},
-                                        {'1', '1'},
-                                        {'6', '9'},
-                                        {'8', '8'},
-                                        {'9', '6'}};
+        const unordered_map<char, char> um = {{'0', '0'},
+                                              {'1', '1'},
+                                              {'6', '9'},
+                                              {'8', '8'},
+                                              {'9', '6'}};
         for(int i = 0; i < (num.size() + 1) / 2; i++) {
             if(!um.count(num[i])) 
                 return false;
-            if(um[num[i]] != num[num.size()-1-i]) 
+            if(um.at(num[i]) != num[num.size()-1-i]) 
                 return false;
         }
         return true;
