0fff9ba1b8d8d32aebf1b8681760c0a768f05f80
BEaFAN <ziqifan16@gmail.com>
2020-07-14 22:34:51 -0700

    Update 78.subsets.cpp

--- fb/78.subsets.cpp
+++ fb/78.subsets.cpp
@@ -1,3 +1,24 @@
+// Bit mask
+// Time O(2^n)
+// Space O(n)
+class Solution {
+public:
+  vector<vector<int>> subsets(vector<int>& nums) {
+    const int n = nums.size();
+    vector<vector<int>> ans;    
+    for (int s = 0; s < 1 << n; ++s) {
+      vector<int> cur;
+      for (int i = 0; i < n; ++i)
+        if (s & (1 << i)) cur.push_back(nums[i]);
+      ans.push_back(cur);
+    }
+    return ans;
+  }
+};
+
+// DFS
+// Time O(2^n)
+// Space O(n)
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
