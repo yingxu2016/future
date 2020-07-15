1b8b7926cfc0328bacae525a4d7ac2f8152f0249
BEaFAN <ziqifan16@gmail.com>
2020-07-14 17:00:06 -0700

    Update 347.Top-K-Frequent-Elements.cpp

--- fb/347.Top-K-Frequent-Elements.cpp
+++ fb/347.Top-K-Frequent-Elements.cpp
@@ -1,3 +1,5 @@
+// Time O(n*logk)
+// Space O(n)
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -27,6 +29,33 @@ public:
             pq.pop();
         }
         
+        return ans;
+    }
+};
+
+// Butcket sort
+// Time O(n)
+// Space O(n)
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        int max_freq = 1;
+        for (const int num : nums)
+            max_freq = max(max_freq, ++count[num]);
+        unordered_map<int, vector<int>> buckets;
+        for (const auto& kv : count)
+            buckets[kv.second].push_back(kv.first);
+        vector<int> ans;
+        for (int i = max_freq; i >= 1; --i) {
+            auto it = buckets.find(i);
+            if (it == buckets.end()) continue;
+            for(auto i : it->second) {
+                ans.push_back(i);
+                if(ans.size() == k) 
+                    return ans;
+            }
+        }
         return ans;
     }
 };
