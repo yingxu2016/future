fbabe9b367adb798580f0fb663864f9d37940c5c
BEaFAN <ziqifan16@gmail.com>
2020-07-15 21:25:43 -0700

    Create 14.Longest-Common-Prefix.cpp

---
+++ fb/14.Longest-Common-Prefix.cpp
@@ -0,0 +1,16 @@
+// vertical scan
+// Time O(S), where S is total num of chars in strs
+// Space O(1)
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string ans;
+        for (int i = 0; i < strs[0].size(); ++i) {
+            for (const string& s : strs)
+                if (s.length() <= i || s[i] != strs[0][i]) return ans;        
+            ans += strs[0][i];
+        }
+        return ans;
+    }
+};
