d6583b053936015a9d641217cab5f0e35bb676d2
BEaFAN <ziqifan16@gmail.com>
2020-07-16 22:30:25 -0700

    Create 678.Valid-Parenthesis-String.cpp

---
+++ fb/678.Valid-Parenthesis-String.cpp
@@ -0,0 +1,30 @@
+// https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-678-valid-parenthesis-string/
+// Time O(n^3)
+// Space O(n^2)
+class Solution {
+public:
+    bool checkValidString(const string& s) {
+        int l = s.length();
+        m_ = vector<vector<int>>(l, vector<int>(l, -1));
+        return isValid(s, 0, l - 1);
+    }
+private:
+    vector<vector<int>> m_;
+    bool isValid(const string& s, int i, int j) {
+        if (i > j) return true;
+        if (m_[i][j] >= 0) return m_[i][j];        
+        
+        if (i == j) return m_[i][j] = (s[i] == '*');
+        
+        if ((s[i] == '(' || s[i] == '*')
+          &&(s[j] == ')' || s[j] == '*')
+          && isValid(s, i + 1, j - 1))
+                return m_[i][j] = 1;        
+        
+        for (int p = i; p < j; ++p)
+            if (isValid(s, i, p) && isValid(s, p + 1, j))
+                return m_[i][j] = 1;                        
+        
+        return m_[i][j] = 0;
+    }
+};
