bbd1c151826e59107be2ed8b0760694256001e37
BEaFAN <ziqifan16@gmail.com>
2020-07-17 21:24:16 -0700

    Update 74.Search-a-2D-Matrix.cpp

--- fb/74.Search-a-2D-Matrix.cpp
+++ fb/74.Search-a-2D-Matrix.cpp
@@ -23,3 +23,44 @@ public:
         return false;
     }
 };
+
+// Time O(logm + logn)
+// Space O(1)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) return false;
+        
+        int l = 0;
+        int r = matrix.size() - 1;
+        int find = -1;
+        while(l <= r) {
+            int m = l + (r - l) / 2;
+            if((matrix[m][0] <= target) && 
+               (matrix[m][matrix[0].size() - 1] >= target)) {
+                find = m;
+                break;
+            }
+            if(matrix[m][0] > target)
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        if(find == -1)
+            return false;
+        
+        l = 0;
+        r = matrix[0].size() - 1;
+        while(l <= r) {
+            int m = l + (r - l) / 2;
+            if(matrix[find][m] == target)
+               return true;
+            if(matrix[find][m] > target)
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        
+        return false;
+    }
+};
