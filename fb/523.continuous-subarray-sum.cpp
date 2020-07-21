d6c7922896e5d31acf8515b39908136038f5f9ed
BEaFAN <ziqifan16@gmail.com>
2020-07-20 22:14:12 -0700

    Update 523.continuous-subarray-sum.cpp

--- fb/523.continuous-subarray-sum.cpp
+++ fb/523.continuous-subarray-sum.cpp
@@ -1,22 +1,32 @@
+// Time O(|nums|)
+// Space O(min(|nums|, k))
+/**
+ * a % k = x
+ * b % k = x
+ * (a - b) % k = x - x = 0
+ * here a - b = the sum between i and j
+ */
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        if(nums.empty()) return false;
-        unordered_map<int, int> sum2Idx;
         int sum = 0;
-        sum2Idx[sum] = -1;
+        
+        // key is sum % k, value is the corresponding index
+        unordered_map<int, int> um;
+        um[0] = -1; // in case the satisfying sequence starting from index 0
+        
         for(int i = 0; i < nums.size(); i++) {
             sum += nums[i];
             if(k != 0)
-                sum %= k;
-            if(sum2Idx.count(sum)) {
-                if(i - sum2Idx[sum] >= 2) {
+                sum = sum % k;
+            if(um.count(sum)) {
+                if(i - um[sum] >= 2) // Q askes sequence len >= 2
                     return true;
-                }
+            } else {
+                um[sum] = i;
             }
-            else
-                sum2Idx[sum] = i;
         }
+        
         return false;
     }
 };
