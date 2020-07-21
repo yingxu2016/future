c96c2b06c5852b5de73a0a3b54b1fe61243a0c4f
BEaFAN <ziqifan16@gmail.com>
2020-07-20 22:41:57 -0700

    Create 7.Reverse-Integer.cpp

---
+++ fb/7.Reverse-Integer.cpp
@@ -0,0 +1,18 @@
+// Time O((log10)X)
+// Space O(1)
+class Solution {
+public:
+    int reverse(int x) {
+        int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            // 7 and -8 due to int range [-2147483648, 2147483647]
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+};
