c91bd13665a2391bd89a7d12182772d808e909eb
BEaFAN <ziqifan16@gmail.com>
2020-07-21 22:03:21 -0700

    Create 340.Longest-Substring-with-At-Most-K-Distinct-Characters.cpp

---
+++ fb/340.Longest-Substring-with-At-Most-K-Distinct-Characters.cpp
@@ -0,0 +1,20 @@
+// Two pass. 
+// Note: strict one pass solution see LC solution Approach 2: Sliding Window + Ordered Dictionary.
+// Time O(|s|)
+// Space O(k)
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int res = 0, left = 0;
+        unordered_map<char, int> m;
+        for (int i = 0; i < s.size(); ++i) {
+            ++m[s[i]];
+            while (m.size() > k) {
+                if (--m[s[left]] == 0) m.erase(s[left]);
+                ++left;
+            }
+            res = max(res, i - left + 1);
+        }
+        return res;
+    }
+};
