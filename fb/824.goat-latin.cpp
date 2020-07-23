b8722225f1137c93423d1c6f27f4ceb48465edec
BEaFAN <ziqifan16@gmail.com>
2020-07-22 20:58:44 -0700

    Update 824.goat-latin.cpp

--- fb/824.goat-latin.cpp
+++ fb/824.goat-latin.cpp
@@ -1,33 +1,24 @@
-// Take away: c++ string constrcutor - string(size_t n, char c)
+// Time O(n)
+// Space O(n)
 class Solution {
-public:
+const unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u',
+                                     'A', 'E', 'I', 'O', 'U'};
+
+ public:
     string toGoatLatin(string S) {
-        if(S.empty()) return string{};
-        istringstream iss(S);
-        vector<string> vs;
-        string s;
-        while(iss >> s) {
-            vs.push_back(s);
+        string ans, tok, suff;
+        istringstream ss(S);
+
+        while (ss >> tok) {
+            suff += 'a';
+
+            if (!vowels.count(tok[0]))
+                tok = tok.substr(1) + tok[0];
+
+            ans += tok + "ma" + suff + ' ';
         }
-        string ans = "";
-        int size = vs.size();
-        for(int i = 0; i < size; i++) {
-            if(vs[i][0] == 'a' ||
-                    vs[i][0] == 'e' ||
-                    vs[i][0] == 'i' ||
-                    vs[i][0] == 'o' ||
-                    vs[i][0] == 'u' ||
-                    vs[i][0] == 'A' ||
-                    vs[i][0] == 'E' ||
-                    vs[i][0] == 'I' ||
-                    vs[i][0] == 'O' ||
-                    vs[i][0] == 'U') {
-                ans += vs[i] + "ma" + string(i + 1, 'a') + " "; // note the string constructor
-            }
-            else {
-                ans += vs[i].substr(1) + vs[i].substr(0, 1) + "ma" + string(i + 1, 'a') + " ";
-            }
-        }
-        return ans.substr(0, ans.size() - 1);
+
+        ans.pop_back();    // remove extra space
+        return ans;
     }
 };
