e2f787d05c7877257e183f917691ab4b7da1df18
BEaFAN <ziqifan16@gmail.com>
2020-07-27 10:58:47 -0700

    Create 172.Factorial-Trailing-Zeroes.cpp

---
+++ fb/172.Factorial-Trailing-Zeroes.cpp
@@ -0,0 +1,16 @@
+// Time O(logn)
+// Space O(1)
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int zeroCount = 0;
+        // We need to use long because currentMultiple can potentially become
+        // larger than an int.
+        long currentMultiple = 5;
+        while (n >= currentMultiple) {
+            zeroCount += (n / currentMultiple);
+            currentMultiple *= 5;
+        }
+        return zeroCount;
+    }
+};
