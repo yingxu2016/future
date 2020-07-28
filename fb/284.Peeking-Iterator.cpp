124c7f4477f55a7999275f4f72e747297612385a
BEaFAN <ziqifan16@gmail.com>
2020-07-27 22:05:48 -0700

    Update 284.Peeking-Iterator.cpp

--- fb/284.Peeking-Iterator.cpp
+++ fb/284.Peeking-Iterator.cpp
@@ -8,13 +8,13 @@ public:
 	    // Initialize any member here.
 	    // **DO NOT** save a copy of nums and manipulate it directly.
 	    // You should only use the Iterator interface methods.
-        hn = Iterator::hasNext();    
-        pk = Iterator::next(); 
+		hn = Iterator::hasNext();    
+		pk = Iterator::next(); 
 	}
 	
     // Returns the next element in the iteration without advancing the iterator.
 	int peek() {
-        return pk;  
+     	return pk;  
 	}
 	
 	// hasNext() and next() should behave the same as in the Iterator interface.
