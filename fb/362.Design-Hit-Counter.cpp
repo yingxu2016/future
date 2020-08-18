87c76286431c3b8efe6d2391fea4deebdf67c2cf
BEaFAN <ziqifan16@gmail.com>
2020-08-17 22:10:32 -0700

    Create 362.Design-Hit-Counter.cpp

---
+++ fb/362.Design-Hit-Counter.cpp
@@ -0,0 +1,70 @@
+// Time O(n)
+// Space O(n)
+class HitCounter {
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {}
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        q.push(timestamp);
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        while (!q.empty() && timestamp - q.front() >= 300) {
+            q.pop();
+        }
+        return q.size();
+    }
+
+private:
+    queue<int> q;
+};
+
+// Time O(1)
+// Space O(1)
+class HitCounter {
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {
+        times.resize(300);
+        hits.resize(300);
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        int idx = timestamp % 300;
+        if (times[idx] != timestamp) {
+            times[idx] = timestamp;
+            hits[idx] = 1;
+        } else {
+            ++hits[idx];
+        }
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        int res = 0;
+        for (int i = 0; i < 300; ++i) {
+            if (timestamp - times[i] < 300) {
+                res += hits[i];
+            }
+        }
+        return res;
+    }
+
+private:
+    vector<int> times, hits;
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
