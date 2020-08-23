f1c596fc9e1bc76217452dd57de166a7868e377f
BEaFAN <ziqifan16@gmail.com>
2020-08-22 22:19:41 -0700

    Update 17.letter-combinations-of-a-phone-number.cpp

--- fb/17.letter-combinations-of-a-phone-number.cpp
+++ fb/17.letter-combinations-of-a-phone-number.cpp
@@ -1,6 +1,9 @@
-// Author: Huahua
-// DFS
-// Runtime: 0 ms
+// Time O(3^N×4^M), where N is the number of digits in the input that maps to 
+// 3 letters (e.g. 2, 3, 4, 5, 6, 8) and M is the number of digits in the input
+// that maps to 4 letters (e.g. 7, 9), and N+M is the total number digits in the input.
+// Space O(3^N×4^M)
+
+// DFS/backtracking
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
@@ -39,9 +42,8 @@ private:
     }
 };
 
-// Author: Huahua
 // BFS
-// Runtime: 3 ms
+// Space O(1)
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
