18d6c01c944d2c5da6ca0c1c358b74e610aefb4e
BEaFAN <ziqifan16@gmail.com>
2020-08-25 22:59:02 -0700

    Create 757.Set-Intersection-Size-At-Least-Two.cpp

---
+++ fb/757.Set-Intersection-Size-At-Least-Two.cpp
@@ -0,0 +1,34 @@
+// Time O(n^2)
+// Space O(n)
+class Solution {
+public:
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        if (intervals.size() == 1) {
+            return 2;
+        }
+
+        sort(intervals.begin(), intervals.end(), [](const auto& l, const auto& r) {
+            return l[1] < r[1];
+        });
+
+        vector<int> cur;
+        int res = 0;
+        for (int i = 0; i < intervals.size(); ++i) {
+            if (cur.empty() || cur[1] < intervals[i][0]) {
+                cur = {intervals[i][1] - 1, intervals[i][1]};
+                res += 2;
+            } else {
+                if (cur[0] < intervals[i][0]) {
+                    if (cur[1] == intervals[i][1]) {
+                        cur = {intervals[i][1] - 1, intervals[i][1]};    
+                    } else {
+                        cur = {cur[1], intervals[i][1]};
+                    }
+                    res += 1;
+                }
+            }
+        }
+
+        return res;
+    }
+};
