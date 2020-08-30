ce0557f56c7bb088e89b6d2e1b0fcab83bc4703e
BEaFAN <ziqifan16@gmail.com>
2020-08-30 13:54:11 -0700

    Update 480.Sliding-Window-Median.cpp

--- fb/480.Sliding-Window-Median.cpp
+++ fb/480.Sliding-Window-Median.cpp
@@ -7,13 +7,14 @@ public:
         vector<double> ans;
         if (k == 0) return ans;
         vector<int> window(nums.begin(), nums.begin() + k - 1);
-        std::sort(window.begin(), window.end());
+        sort(window.begin(), window.end());
         for (int i = k - 1; i < nums.size(); ++i) {
             window.push_back(nums[i]);
             auto it = prev(window.end(), 1);
-            auto const insertion_point = std::upper_bound(window.begin(), it, *it);             std::rotate(insertion_point, it, it + 1);          
+            auto const insertion_point = upper_bound(window.begin(), it, *it);             
+            rotate(insertion_point, it, it + 1);          
             ans.push_back((static_cast<double>(window[k / 2]) + window[(k - 1) / 2]) / 2);
-            window.erase(std::find(window.begin(), window.end(), nums[i - k + 1]));      
+            window.erase(find(window.begin(), window.end(), nums[i - k + 1]));      
         }
         return ans;
     }
