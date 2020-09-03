6bf63b8c663ec333187e34b0a04f3bdafbc0f17c
BEaFAN <ziqifan16@gmail.com>
2020-09-02 22:43:46 -0700

    Update 283.move-zeroes.cpp

--- fb/283.move-zeroes.cpp
+++ fb/283.move-zeroes.cpp
@@ -1,9 +1,11 @@
+// Time O(n)
+// Space O(1)
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for(int i = 0, lastNonZeroFoundAt = 0; i < nums.size(); i++) {
+        for(int i = 0, startIdxOfConsecutiveZeros = 0; i < nums.size(); i++) {
             if(nums[i] != 0) {
-                swap(nums[lastNonZeroFoundAt++], nums[i]);
+                swap(nums[startIdxOfConsecutiveZeros++], nums[i]);
             }
         }
     }
