eaf63fac3ea62e5d9182edc5a2b4c2cc1cc348c8
BEaFAN <ziqifan16@gmail.com>
2020-10-03 20:41:28 -0700

    Update 157.read-n-characters-given-read4.cpp

--- fb/157.read-n-characters-given-read4.cpp
+++ fb/157.read-n-characters-given-read4.cpp
@@ -4,27 +4,6 @@ class Solution {
 public:
     /**
      * @param buf Destination buffer
-     * @param n   Maximum number of characters to read
-     * @return    The number of characters read
-     */
-    int read(char *buf, int n) {
-        if(!buf || n <= 0) return 0;
-        int ans = 0;
-        while(n > 0) {
-            int out = read4(buf + ans);
-            if(out == 0) break;
-            out = min(n, out);
-            n -= out;
-            ans += out;
-        }
-        return ans;
-    }
-};
-
-class Solution {
-public:
-    /**
-     * @param buf Destination buffer
      * @param n   Number of characters to read
      * @return    The number of actual characters read
      */
