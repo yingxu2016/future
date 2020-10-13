239ee49b34b164cd2e5c480f0ffc9bbfc8ab309a
BEaFAN <ziqifan16@gmail.com>
2020-10-12 20:50:36 -0700

    Update 896.Monotonic-Array.cpp

--- fb/896.Monotonic-Array.cpp
+++ fb/896.Monotonic-Array.cpp
@@ -22,3 +22,22 @@ public:
         return true;
     }
 };
+
+// Time O(n)
+// Space O(1)
+// LC solution
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+        bool increasing = true;
+        bool decreasing = true;
+        for (int i = 0; i < A.size() - 1; ++i) {
+            if (A[i] > A[i+1])
+                increasing = false;
+            if (A[i] < A[i+1])
+                decreasing = false;
+        }
+
+        return increasing || decreasing;
+    }
+};
