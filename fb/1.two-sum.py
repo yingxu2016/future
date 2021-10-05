042ab8d3640f4759148acf1fb9cafab26770fa87
PeachPie <18142416+yingxu2016@users.noreply.github.com>
2021-10-04 22:43:20 -0700

    Update 1.two-sum.py

--- fb/1.two-sum.py
+++ fb/1.two-sum.py
@@ -1,6 +1,7 @@
 class Solution:
   def twoSum(self, nums: List[int], target: int) -> List[int]:
     hashmap = {}
+    
     for i in range(len(nums)):
       complement = target - nums[i]
       if complement in hashmap and hashmap[complement] != i:
