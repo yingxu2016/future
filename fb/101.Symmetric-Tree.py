be25b3f4fda34db75b19a49439e0f870a3ad4c85
PeachPie <18142416+yingxu2016@users.noreply.github.com>
2021-10-05 22:30:02 -0700

    Update 101.Symmetric-Tree.py

--- fb/101.Symmetric-Tree.py
+++ fb/101.Symmetric-Tree.py
@@ -15,3 +15,20 @@ class Solution:
             return left.val == right.val and isMirror(left = left.left, right = right.right) and isMirror(left = left.right, right = right.left)
         
         return isMirror(left = root, right = root)
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        queue = [root, root]
+        while len(queue) > 0:
+            left = queue.pop(0)
+            right = queue.pop(0)
+            if left is None and right is None:
+                continue
+            if left is None or right is None:
+                return False
+            if left.val != right.val:
+                return False
+            
+            queue = queue + [left.left, right.right, left.right, right.left]
+            
+        return True
