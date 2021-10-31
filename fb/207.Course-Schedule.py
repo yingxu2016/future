a146ba04ffbbb65145bb95712187d1fc5f88c163
PeachPie <18142416+yingxu2016@users.noreply.github.com>
2021-10-31 09:51:44 -0700

    Create 207.Course-Schedule.py

---
+++ fb/207.Course-Schedule.py
@@ -0,0 +1,24 @@
+// Topolical sort
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_list = defaultdict(list)
+        indegree = {}
+        
+        for dest, src in prerequisites:
+            adj_list[src].append(dest)
+            indegree[dest] = indegree.get(dest, 0) + 1
+        
+        topolocial_sorted_set = []
+        zero_indegree_queue = deque([v for v in range(numCourses) if v not in indegree])
+        
+        while zero_indegree_queue:
+            vertex = zero_indegree_queue.popleft()
+            topolocial_sorted_set.append(vertex)
+            if adj_list.get(vertex):
+                for neighbor in adj_list[vertex]:
+                    indegree[neighbor] -= 1
+                    
+                    if indegree[neighbor] == 0:
+                        zero_indegree_queue.append(neighbor)
+        
+        return len(topolocial_sorted_set) == numCourses
